7d99756b309994654b472f7e402bf59aa1314e54
morgan-783 <Morgeek523@knights.ucf.edu>
2022-04-17 02:32:35 -0400

    Add Function to Script

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 'use strict';
 
-//Function that randomly returns R/P/S
+//Starting Conditions
 const options = ['rock', 'paper', 'scissors'];
-
+//Function that randomly returns R/P/S
 const computerPlay = function () {
   return options[Math.floor(Math.random() * options.length)];
 };
 computerPlay();
 
 //Play a single round
+const playRound = function (playerSelection, computerSelection) {
+  //Use an if/else statement
+  if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    console.log(`You won 💯! ${playerSelection} beats ${computerSelection}`);
+  } else if (playerSelection === computerSelection) {
+    console.log(`It's a tie!`);
+  } else {
+    console.log(`You lose 😞! ${computerSelection} beats ${playerSelection}`);
+  }
+};
+const computerSelection = computerPlay();
+const playerSelection = 'rock';
+console.log(playRound(playerSelection, computerSelection));
